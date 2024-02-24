ecc24297445a566fdfc238cb0929583536aa189f
luciandev012 <thanguk04@gmail.com>
2024-02-24 19:33:43 +0700

    complete category page

--- DiamondProject/ClientApp/src/views/Client/RingCategory/ListRingFilter.js
+++ DiamondProject/ClientApp/src/views/Client/RingCategory/ListRingFilter.js
@@ -3,27 +3,32 @@ import { Link } from "react-router-dom";
 import { formatMoney } from "../../../helper/helper";
 
 export default function ListRingFilter({ rings }) {
-  return rings.map((ring) => {
-    return (
-      <div className="col-md-3 product" id="product" key={ring.ringId}>
-        <img
-          src="/images/diamond_rings.png"
-          alt="img"
-          className="card-product card-product-front"
-          id={ring.ringId}
-        />
-        <div className="card-product card-product-back">
-          <div className="back-content">
-            <h5>{ring.ringName}</h5>
-            <p className="back-title">Giá</p>
-            <p>{formatMoney(ring.price)}</p>
-            <p className="back-title">Chất liệu</p>
-            <p>{ring.material}</p>
+  return rings
+    ? rings.map((ring) => {
+        return (
+          <div className="col-md-3 product" id="product" key={ring.ringId}>
+            <img
+              src="/images/diamond_rings.png"
+              alt="img"
+              className="card-product card-product-front"
+              id={ring.ringId}
+            />
+            <div className="card-product card-product-back">
+              <div className="back-content">
+                <h5>{ring.ringName}</h5>
+                <p className="back-title">Giá</p>
+                <p>{formatMoney(ring.price)}</p>
+                <p className="back-title">Chất liệu</p>
+                <p>{ring.material}</p>
+              </div>
+              <button className="discover-button">Xem chi tiết</button>
+            </div>
+            <Link
+              className="absolute-link"
+              to={`/detail-product/${ring.ringId}`}
+            />
           </div>
-          <button className="discover-button">Xem chi tiết</button>
-        </div>
-        <Link className="absolute-link" to={`/detail-product/${ring.ringId}`} />
-      </div>
-    );
-  });
+        );
+      })
+    : null;
 }
--- DiamondProject/ClientApp/src/views/Client/RingCategory/RingCategory.js
+++ DiamondProject/ClientApp/src/views/Client/RingCategory/RingCategory.js
@@ -13,14 +13,28 @@ export default function RingCategory() {
   const params = useParams();
   const dispatch = useDispatch();
   const [category, setCategory] = useState(null);
+  const [rings, setRings] = useState([]);
   const [toggleArrow, setToggleArrow] = useState(false);
   const [filter, setFilter] = useState(0);
+
   const [madeIn, setMadeIn] = useState([]);
-  const [selectedFilter, setSelectedFilter] = useState([]);
+  const [selectedMadeIn, setSelectedMadeIn] = useState([]);
+
+  const [material, setMaterial] = useState([]);
+  const [selectedMaterial, setSelectedMaterial] = useState([]);
+
+  const [arrange, setArrange] = useState(0);
+  const listArrange = [
+    { value: 0, text: "-- Chọn tiêu chí --" },
+    { value: 1, text: "Giá tăng dần" },
+    { value: 2, text: "Giá giảm dần" },
+  ];
   //const [productWidth, setProductWidth] = useState("initial");
   useEffect(() => {
     const fetchCategory = async () => {
       let temp = await dispatch(getRingCategory(params.id));
+
+      // setMadeInArray
       let madeInFlag = {};
       temp.rings.forEach((ring) => {
         if (madeInFlag[ring.madeIn] !== undefined) {
@@ -29,26 +43,104 @@ export default function RingCategory() {
           madeInFlag[ring.madeIn] = 1;
         }
       });
-      console.log(madeInFlag);
       setMadeIn(Object.entries(madeInFlag));
 
+      // setMaterialArray
+      let materialFlag = {};
+      temp.rings.forEach((ring) => {
+        if (materialFlag[ring.material] !== undefined) {
+          materialFlag[ring.material]++;
+        } else {
+          materialFlag[ring.material] = 1;
+        }
+      });
+      setMaterial(Object.entries(materialFlag));
+
       setCategory(temp);
+      setRings(temp.rings);
     };
-    // const resizeWindows = () => {
-    //   let curWidth = document.getElementById("product");
-    //   let width = curWidth.clientWidth + "px";
-    //   setProductWidth(width);
-    // };
-    // window.addEventListener("load", resizeWindows);
-    // window.addEventListener("resize", resizeWindows);
+
     fetchCategory();
   }, [params]);
+
+  useEffect(() => {
+    filterBySelected();
+  }, [selectedMadeIn, selectedMaterial]);
 
   const handleClickSlideDescription = () => {
     setToggleArrow(!toggleArrow);
   };
-  const handleCheckMadeIn = (mi) => {
-    setSelectedFilter((prev) => [...prev, mi]);
+  const handleCheckMadeIn = (mi, e) => {
+    setFilter(0);
+    if (e.target.checked) {
+      setSelectedMadeIn((prev) => [...prev, mi[0]]);
+    } else {
+      setSelectedMadeIn((prev) => prev.filter((item) => item !== mi[0]));
+    }
+    //filterBySelected();
+  };
+
+  const handleDeleteMadeIn = (val) => {
+    setSelectedMadeIn((prev) => prev.filter((item) => item !== val));
+    //filterBySelected();
+  };
+
+  const handleCheckMaterial = (ma, e) => {
+    setFilter(0);
+    if (e.target.checked) {
+      setSelectedMaterial((prev) => [...prev, ma[0]]);
+    } else {
+      setSelectedMaterial((prev) => prev.filter((item) => item !== ma[0]));
+    }
+  };
+  const handleDeleteMaterial = (val) => {
+    setSelectedMaterial((prev) => prev.filter((item) => item !== val));
+  };
+
+  const filterBySelected = () => {
+    //let temp = rings;
+    let filterResult = [];
+    let madeInResult = [];
+    selectedMadeIn.forEach((item) => {
+      let temp = category.rings.filter((ring) => ring.madeIn === item);
+      madeInResult = [...madeInResult, ...temp];
+    });
+    selectedMaterial.forEach((item) => {
+      let temp = category.rings.filter((ring) => ring.material === item);
+      filterResult = [...filterResult, ...temp];
+    });
+    if (selectedMadeIn.length > 0 && selectedMaterial.length > 0) {
+      let temp = [...madeInResult, ...filterResult];
+      //console.log();
+      temp = temp.sort((a, b) => a.ringId.localeCompare(b.ringId));
+      let result = [];
+      for (let index = 0; index < temp.length - 1; index++) {
+        if (temp[index].ringId === temp[index + 1].ringId) {
+          result.push(temp[index]);
+        }
+      }
+      setRings(result);
+    } else if (selectedMadeIn.length > 0) {
+      setRings(madeInResult);
+    } else if (selectedMaterial.length > 0) {
+      setRings(filterResult);
+    } else {
+      setRings(category && category.rings);
+    }
+  };
+  const handleChangeSort = (e) => {
+    setArrange(e.target.value);
+    switch (e.target.value) {
+      case "1":
+        setRings((prev) => prev.sort((a, b) => a.price - b.price).slice());
+        break;
+      case "2":
+        setRings((prev) => prev.sort((a, b) => b.price - a.price).slice());
+        break;
+      default:
+        setRings(category.rings);
+        break;
+    }
   };
 
   return category ? (
@@ -96,7 +188,7 @@ export default function RingCategory() {
                 <Icon icon="icons8:angle-down" fontSize={25} />
               </button>
             </span>
-            <span>
+            {/* <span>
               Bộ sưu tập{" "}
               <button
                 onClick={() => setFilter((prev) => (prev !== 3 ? 3 : 0))}
@@ -105,39 +197,75 @@ export default function RingCategory() {
               >
                 <Icon icon="icons8:angle-down" fontSize={25} />
               </button>
-            </span>
+            </span> */}
           </div>
           <div className="sort">
             <span>Sắp xếp: </span>
-            <select>
-              <option>Giá từ thấp đến cao</option>
-              <option>Giá từ cao đến thấp</option>
-              <option>Hàng mới về</option>
+            <select onChange={handleChangeSort} value={arrange}>
+              {listArrange.map((item, index) => {
+                return (
+                  <option value={item.value} key={index}>
+                    {item.text}
+                  </option>
+                );
+              })}
             </select>
           </div>
         </div>
       </div>
-      <div>
-        <div className={`filter-option follow ${filter !== 0 ? "active" : ""}`}>
+      <div className="relative-filter">
+        <div className="selected-filter-list">
+          {selectedMadeIn.length > 0
+            ? selectedMadeIn.map((select, index) => {
+                return (
+                  <span className="selected-filter" key={index}>
+                    <button onClick={() => handleDeleteMadeIn(select)}>
+                      ✖️
+                    </button>
+                    &nbsp;{select}
+                  </span>
+                );
+              })
+            : null}
+          {selectedMaterial.length > 0
+            ? selectedMaterial.map((select, index) => {
+                return (
+                  <span className="selected-filter" key={index}>
+                    <button onClick={() => handleDeleteMaterial(select)}>
+                      ✖️
+                    </button>
+                    &nbsp;{select}
+                  </span>
+                );
+              })
+            : null}
+        </div>
+        <div className={`filter-option ${filter !== 0 ? "active" : ""}`}>
           <div className="option">
-            {selectedFilter.length > 0
-              ? selectedFilter.map((select, index) => {
-                  return (
-                    <span className="selected-filter" key={index}>
-                      <button>✖️</button>&nbsp;{select[0]}
-                    </span>
-                  );
-                })
-              : madeIn
+            {madeIn && filter === 1
               ? madeIn.map((mi, index) => {
                   return (
                     <div key={index} className="option-item">
                       <Checkbox
-                        checked={false}
-                        onChange={() => handleCheckMadeIn(mi)}
+                        checked={selectedMadeIn.includes(mi[0])}
+                        onChange={(e) => handleCheckMadeIn(mi, e)}
                         inputProps={{ "aria-label": "controlled" }}
                       />{" "}
                       {`${mi[0]} (${mi[1]})`}
+                    </div>
+                  );
+                })
+              : null}
+            {material && filter === 2
+              ? material.map((ma, index) => {
+                  return (
+                    <div key={index} className="option-item">
+                      <Checkbox
+                        checked={selectedMaterial.includes(ma[0])}
+                        onChange={(e) => handleCheckMaterial(ma, e)}
+                        inputProps={{ "aria-label": "controlled" }}
+                      />{" "}
+                      {`${ma[0]} (${ma[1]})`}
                     </div>
                   );
                 })
@@ -146,7 +274,7 @@ export default function RingCategory() {
         </div>
       </div>
       <div className="follow list-product row">
-        <ListRingFilter rings={category.rings} />
+        <ListRingFilter rings={rings} />
       </div>
     </section>
   ) : (
